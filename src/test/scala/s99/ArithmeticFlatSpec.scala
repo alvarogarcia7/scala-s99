ad68541dd7f73ecbc78ee8f81d742fa8492559dc
Alvaro Garcia <derecerca@gmail.com>
2014-06-06 06:20:30 +0200

    add scalatest, format pom

---
+++ src/test/scala/s99/ArithmeticFlatSpec.scala
@@ -0,0 +1,26 @@
+package s99;
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import scala.collection.mutable.Stack;
+
+@RunWith(classOf[JUnitRunner])
+class StackSpec extends FlatSpec {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    assert(stack.pop === 2)
+    assert(stack.pop === 1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new Stack[String]
+    intercept[NoSuchElementException] {
+      emptyStack.pop
+    }
+  }
+}
