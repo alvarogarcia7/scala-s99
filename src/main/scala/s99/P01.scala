7ed4588b9574ea3026d450229fcf9e65f77df874
Alvaro Garcia <derecerca@gmail.com>
2014-06-06 19:39:30 +0200

    refactor: accept any type (based on solution in http://aperiodic.net/phil/scala/s-99/p01.scala)

--- src/main/scala/s99/P01.scala
+++ src/main/scala/s99/P01.scala
@@ -4,7 +4,7 @@ import scala.collection.mutable.LinkedList
 
 object P01 {
 
-  def last(list: List[Int]): Int = list match {
+  def last[T](list: List[T]): T = list match {
     case head :: Nil => head
     case _ :: tail => last(tail)
     case _ => throw new NoSuchElementException
